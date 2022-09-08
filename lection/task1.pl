25e8cae1f8060ec538d8e9c2161d789bafbf05d8
Dima Vladimirov <diman_vladimirov34@mail.ru>
2022-09-09 02:18:35 +0300

    Add files via upload

---
+++ lection/task1.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+#6 таск
+
+$a = "abtt";
+$b = "abtt";
+$\ = "\n";
+
+print "Value of \$a = $a \nand value of \$b = $b\n";
+
+if ($a gt $b) {
+	print "$a gt \$b is true\n";
+} else {
+	print "$a gt \$b is false\n";
+}
+
+if ($a lt $b) {
+	print "$a lt \$b is true\n";
+} else {
+	print "$a lt \$b is false\n";
+}
+
+if ($a ge $b) {
+	print "$a ge \$b is true\n";
+} else {
+	print "$a ge \$b is false\n";
+}
+
+if ($a le $b) {
+	print "$a le \$b is true\n";
+} else {
+	print "$a le \$b is false\n";
+}
+
+if ($a eq $b) {
+	print "$a eq \$b is true\n";
+} else {
+	print "$a eq \$b is false\n";
+}
+
+if ($a ne $b) {
+	print "$a ne \$b is true\n";
+} else {
+	print "$a ne \$b is false\n";
+}
