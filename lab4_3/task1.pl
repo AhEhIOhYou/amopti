4f57d7ce9cdb839282a1d51eccc27e1306c3f787
Dima Vladimirov <diman_vladimirov34@mail.ru>
2023-02-16 22:11:50 +0300

    Add files via upload

---
+++ lab4_3/task1.pl
@@ -0,0 +1,226 @@
+#!/bin/perl
+
+use Data::Dumper qw(Dumper);
+
+my $fileOpenError = "file open error";
+my $fh;
+
+my $fileName = "readers.txt";
+my $headings = ();
+my $records = ();
+
+
+sub CheckData {
+	return (defined($headings) && defined($records))
+}
+sub LoadDataFromFile {
+	local $inText = "";
+
+	open $fh, '<', $fileName || die $fileOpenError;
+	while (local $str = <$fh>)
+	{
+		$inText .= $str;
+	}
+	close $fh;
+
+	local @arrData = split /\n/, $inText;
+	local @arrHeadings = split /\s+/, @arrData[0];
+	$headings = \@arrHeadings;
+
+	local @arrRecords = ();
+	shift @arrData;
+
+	for my $i (0 .. $#arrData)
+	{
+		my @dataParts = split /\s+/, @arrData[$i];
+
+		for my $j (0 .. $#dataParts)
+		{
+			$arrRecords[$i][$j] = $dataParts[$j];
+		}
+	}
+
+	$records = \@arrRecords;
+	print Dumper $records;
+}
+sub GetTableFormatText {
+
+	if (!CheckData)
+	{
+		print "Error! No data available!\n";
+		return
+	}
+
+	local $text = "";
+
+	foreach $heading (@{$headings})
+	{
+		$text .= $heading . "\t";
+	}
+	$text .= "\n";
+
+	foreach $arrRecord (@{$records})
+	{
+		foreach $recordData (@{$arrRecord})
+		{
+			$text .= $recordData . "\t";
+		}
+		$text .= "\n";
+	}
+
+	return $text;
+}
+sub AddRecord {
+
+	if (!CheckData)
+	{
+		print "Error! No data available!\n";
+		return
+	}
+
+	local @newData;
+
+	for (my $i = 0; $i < $#{$headings} + 1; $i++)
+	{
+		print ${$headings}[$i] . ": ";
+		local $inputValue = <>;
+		chomp $inputValue;
+		$newData[$i] = $inputValue;
+	}
+
+	push @$records, \@newData;
+	print "Successfully added!\n";
+}
+sub SaveToFile {
+
+	if (!CheckData)
+	{
+		print "Error! No data available!\n";
+		return
+	}
+
+	open $fh, '>', $fileName || die $fileOpenError;
+	print $fh GetTableFormatText;
+	close $fh;
+}
+sub DeleteRecordByID {
+
+	if (!CheckData)
+	{
+		print "Error! No data available!\n";
+		return
+	}
+
+	print "ID: ";
+	local $idSearch = <>;
+	chomp $idSearch;
+
+	local $deleteIndex = -1;
+
+	while (my ($i, $element) = each(@{$records}))
+	{
+		if (${$element}[0] eq $idSearch)
+		{
+			$deleteIndex = $i;
+			last;
+		}
+	}
+
+	if ($deleteIndex > -1)
+	{
+		print $deleteIndex . "\n";
+		splice @{$records}, $deleteIndex, 1;
+		print "\"$idSearch\" successfully deleted";
+	}
+	else
+	{
+		print "Error, reader with \"$idSearch\" id doesn't exist\n";
+	}
+}
+sub Search {
+
+	if (!CheckData)
+	{
+		print "Error! No data available!\n";
+		return
+	}
+
+	print "Search: ";
+	local $searchText = <>;
+	chomp $searchText;
+
+	local @indexFound = ();
+
+	while (my ($i, $arrRecord) = each(@{$records}))
+	{
+		foreach $recordData (@{$arrRecord})
+		{
+			if ($recordData =~ /$searchText/)
+			{
+				push @indexFound, $i;
+				last;
+			}
+		}
+	}
+
+	if (@indexFound > 0)
+	{
+		print "FOUND:\n";
+
+		foreach $heading (@{$headings})
+		{
+			print $heading . "\t";
+		}
+		print "\n";
+
+		foreach $index (@indexFound)
+		{
+			for (my $j = 0; $j < $#{$headings} + 1; $j++)
+			{
+				print ${$records}[$index][$j] . "\t";
+			}
+			print "\n";
+		}
+	}
+	else
+	{
+		print "By request \"$searchText\" nothing found\n";
+	}
+}
+
+# todo делать ли?
+# todo nado sdelat
+sub SortByHeading {
+	print "Heading: ";
+	local $searchHeading = <>;
+	chomp $searchHeading;
+}
+
+my $options = "0 - print all data\n1 - add record\n2 - save to file\n3 - dele by name\n4 - search\n5 - load file\n6 - exit\nOption: ";
+print $options;
+while (<>) {
+	my $cm = $_;
+	chomp($cm);
+	if ($cm == 0) {
+		print GetTableFormatText;
+	}
+	if ($cm == 1) {
+		AddRecord;
+	}
+	if ($cm == 2) {
+		SaveToFile;
+	}
+	if ($cm == 3) {
+		DeleteRecordByID;
+	}
+	if ($cm == 4) {
+		Search;
+	}
+	if ($cm == 5) {
+		LoadDataFromFile;
+	}
+	if ($cm == 6) {
+		exit(100);
+	}
+	print $options;
+}
